e9f3f954ada00b0cfc8ac4ac1187aaafb1842200
Ritu-03Thakur <ritutec001@gmail.com>
2022-10-03 15:59:52 +0530

    Layout of Navigation Bar  is done linking with the toogle is pending.

---
+++ pages/main.js
@@ -0,0 +1,5 @@
+// const toogleButton = document.getElementsByClassName('toogle-button')[0];
+// const navbarLinks = document.getElementsByClassName('menu')[0];
+// toogleButton.addEventListener('click' , () => {
+//     navbarLinks.classList.toggle('active');
+// })
